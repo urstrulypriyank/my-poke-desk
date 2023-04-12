ce2e4e2e1d5e0cbf3a1d35511d0a2fffd5449537
priyank rai <0priyankrai@gmail.com>
2023-04-12 10:16:06 +0530

    pagination completed

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -2,15 +2,57 @@ import Image from "next/image";
 import React from "react";
 
 const CardComponent = ({ pokemon }) => {
-  console.log(pokemon);
+  let color = {
+    Normal: "#A8A77A",
+    Fire: "#EE8130",
+    Water: "#6390F0",
+    Electric: "#F7D02C",
+    Grass: "#7AC74C",
+    Ice: "#96D9D6",
+    Fighting: "#C22E28",
+    Poison: "#A33EA1",
+    Ground: "#E2BF65",
+    Flying: "#A98FF3",
+    Psychic: "#F95587",
+    Bug: "#A6B91A",
+    Rock: "#B6A136",
+    Ghost: "#735797",
+    Dragon: "#6F35FC",
+    Dark: "#705746",
+    Steel: "#B7B7CE",
+    Fairy: "#D685AD",
+  };
+  // console.log(pokemon);
   return (
-    <div className=" flex lg:w-[23vw] md:w-[30vw] sm:w-[40vw] w-[90vw] justify-center items-center md:mx-auto ">
-      <img
-        
-        alt={pokemon.name + " image"}
-        src={pokemon.image}
-        className="h-[160px] w-[160px] sm:h-[200px] sm:w-[200px] "
-      />
+    <div className="p-3 shadow-xs rounded-lg shadow-[#f5f5f5]]  flex justify-center max-sm:w-[90vw] ">
+      <div
+        className="
+         bg-white p-7
+         "
+      >
+        <img
+          alt={pokemon.name + " image"}
+          src={pokemon.image}
+          className="h-[160px] w-[160px] sm:h-[200px] sm:w-[200px] bg-gray-300   "
+        />
+        <p className="text-gray-500 opacity-80 font-semibold">{`#${pokemon.number}`}</p>
+        <p className="opacity-80 text-2xl mt-2 font-semibold tracking-wide">
+          {pokemon.name}
+        </p>
+        <div>
+          {pokemon.types.map((type) => {
+            return (
+              <span
+                className=" text-xs text-black/100  font-medium mr-2 px-2.5 py-0.5 rounded  "
+                style={{ backgroundColor: `${color[`${type}`]}` }}
+                key={type}
+              >
+                {type}
+              </span>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/pokemon/index.jsx
+++ src/pages/pokemon/index.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import CardComponent from "../Components/CardComponent";
 import CardComponent from "@/components/CardComponent";
 import { useRouter } from "next/router";
@@ -8,32 +8,76 @@ import { client } from "@/components/appoloClinet";
 import { gql } from "@apollo/client";
 import Navbar from "@/components/Navbar";
 import Layout from "@/components/Layout";
+import { render } from "react-dom";
 
 const Home = ({ pokemons }) => {
-  const [pokemonArrary, setPokemonArray] = useState({ pokemons });
+  const [pokemonArrary, setPokemonArray] = useState(pokemons);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.query({
+          query: gql`
+            query GetPokemons($first: Int!) {
+              pokemons(first: $first) {
+                id
+                number
+                name
+                image
+                types
+              }
+            }
+          `,
+          variables: {
+            first: pageNumber * 20,
+          },
+        });
+        const { data } = response ?? {};
+        const new_data = data.pokemons.slice(
+          pageNumber * 20 - 20,
+          pageNumber * 20
+        );
+        setPokemonArray(new_data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [pageNumber]);
+
   const router = useRouter();
   let routeToPokemonPage = (id) => {
     router.push("/pokemon/" + id);
   };
+
+  // Pagination Funcs
+  function handlePrevious() {
+    setPageNumber((page) => page - 1);
+  }
+  async function handleNext() {
+    setPageNumber(pageNumber + 1);
+  }
+
   return (
     <>
       <Head>
         <title>Pokedex</title>
       </Head>
 
-      <div className="bg-gray-300">
+      <div className="bg-[#f2f2f2]">
         <Navbar />
         <div id="heading " className="flex w-screen items-center flex-col">
           <h1 className="text-center w-screen mt-5 text-2xl">
             Welcome to the Pokedesk
           </h1>
-          <h2 className="italic">Scan Any Pokemon</h2>
+          <h2 className="italic">Clik Any Pokemon to Know More</h2>
         </div>
 
         {/* pokemon list render */}
 
-        <div className="flex flex-wrap justify-around mx-auto items-center w-[95vw] border border-red-500">
-          {pokemons.map((pokemon) => (
+        <div className="flex flex-wrap justify-between  mx-auto items-center w-[90vw] ">
+          {pokemonArrary.map((pokemon) => (
             <div
               onClick={() => routeToPokemonPage(pokemon.id)}
               key={pokemon.id}
@@ -42,6 +86,24 @@ const Home = ({ pokemons }) => {
               <CardComponent pokemon={pokemon} key={pokemon.id} />
             </div>
           ))}
+        </div>
+
+        {/* Button Container */}
+        <div className="flex flex-row  w-[80vw]  justify-between mx-auto m-5 p-5  ">
+          <button
+            className="bg-[#f57522] px-5 py-2 rounded-md font-semibold tracking-wide disabled:bg-gray-500 "
+            onClick={handlePrevious}
+            disabled={pageNumber === 1}
+          >
+            Back
+          </button>
+          <button
+            className="bg-[#f57522] px-5 py-2 rounded-md font-semibold tracking-wide  disabled:bg-gray-500"
+            onClick={handleNext}
+            disabled={pageNumber * 20 >= 151}
+          >
+            Next
+          </button>
         </div>
       </div>
     </>
@@ -52,6 +114,7 @@ export default Home;
 
 export async function getStaticProps() {
   // Fetch data from GraphQL API
+
   const response = await client.query({
     query: gql`
       query GetPokemons {
