a1eae73539aa78dc22cff6ff211b3fb378d21401
priyank rai <0priyankrai@gmail.com>
2023-04-12 18:03:54 +0530

    code refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from "../../public/pokdesk1.png";
 const Navbar = () => {
   return (
     <div className="flex justify-center items-center h-12 p-2 bg-[#f57522]  shadow-md shadow-[#f57522]">
-        <Link href="/pokemon">
+        <Link href="/">
           <Image alt="Pokedex" src={logo} priority width={50} height={50} />
         </Link>
         
